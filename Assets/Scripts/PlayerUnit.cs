9999d56542cb2660c870255898c339bf7dec29a7
hnjog <hnjogo@gmail.com>
2018-10-08 18:18:02 +0900

    불필요한 파일 삭제, 스크립트 정리
    스크립트 정리, 유닛 상태 만들기, 박스 콜라이더 변환하여서 공격체크용으로 사용 예정

--- Assets/Scripts/PlayerUnit.cs
+++ Assets/Scripts/PlayerUnit.cs
@@ -4,63 +4,52 @@ using UnityEngine;
 
 public class PlayerUnit : MonoBehaviour {
 
+    enum State
+    {
+        Move,
+        Attack,
+        Die,
+    }
+
+    State state = State.Move;
+
     [Header("이동 관련 변수")]
-    public float playerUnitSpeed = 1f;                 //플레이어 유닛 이동속도
-    public Rigidbody2D playerUnitRb;                   //플레이어 유닛의 리지드바디
+    public float playerUnitSpeed;                 //플레이어 유닛 이동속도
     
     [Header("스텟 관련 변수")]
-    public int playerUnitHp = 10;                      //플레이어 유닛 체력
-    public int playerUnitAttack = 1;                   //플레이어 유닛 공격력
-    public int playerUnitDepend = 1;                   //플레이어 유닛 방어력
-    public int playerUnitCost = 1;                     //플레이어 유닛 코스트
-    
-
-    [Header("Enemy 관련 변수")]
-    public GameObject enemyUnit;                       //적 유닛
-    public int enemyUnitHp;                            //적 유닛 체력
-    public int enemyUnitAttack;                        //적 유닛 공격력
-    public GameObject enemyNexus;                      //적 넥서스
-    public int enemyNexusHp;                           //적 넥서스 체력
-
-    [Header("Enemy 관련 변수")]
-    public bool playerisDie = false;                   //플레이어 유닛 죽음체크
+    public int playerUnitHp;                       //플레이어 유닛 체력
+    public int playerUnitAttack;                   //플레이어 유닛 공격력
+    public int playerUnitCost;                     //플레이어 유닛 코스트
 
     private void Start()
     {
-        playerUnitRb = GetComponent<Rigidbody2D>();
+        state = State.Move;
+        
     }
 
-    void Update()
+    private void Update()
     {
-        StartCoroutine("Move");
-    }
-
-    //이동
-    IEnumerator Move()
-    {
-        transform.Translate(Vector3.right * playerUnitSpeed * Time.deltaTime);
-        yield return new WaitForSeconds(1f);
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if(other.gameObject.tag == "EnemyUnit")        //적 유닛 체크
+        switch (state)
         {
-            enemyUnit = other.gameObject;
-            //enemyUnitHp = other.GetComponent<EnemyUnit>().enemyUnitHp;
-            //enemyUnitAttack = other.GetComponent<EnemyUnit>().enemyUnitAttack;
-        }
-        else                                           //적 넥서스 체크
-        {
-            enemyNexus = other.gameObject;
-            //enemyNexusHp = other.GetComponent<EnemyNexus>().enemyNexusHp;
+            case State.Move:
+                PlayerUnitMovement();
+                break;
+            case State.Attack:
+                break;
+            case State.Die:
+            default:
+                break;
         }
     }
 
-    IEnumerator Damage()
+    public void PlayerUnitMovement()
     {
-
-        yield return new WaitForSeconds(1f);
+        transform.Translate(Vector2.right * playerUnitSpeed * Time.deltaTime);
+        
     }
 
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        state = State.Attack;
+    }
 }
---
+++ Assets/Scripts/PlayerUnitManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUnitManager : MonoBehaviour
+{
+    [Header("Line 관련 변수")]
+    public Transform[] LineTfs = new Transform[4];
+
+    /// <summary>
+    /// 내부 관리 변수
+    /// </summary>
+    private Vector2        mousePos;                         //마우스 포지션 
+    private RaycastHit2D   rayHit;                           
+    private GameObject     playerUnit;                       //유닛을 저장할 변수
+
+    private void Update()
+    {
+        //마우스 위치
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        //왼쪽 마우스 클릭하는동안
+        if (Input.GetMouseButton(0))
+        {
+            if (rayHit = Physics2D.Raycast(mousePos, Vector2.zero))
+            {
+                //왼쪽 마우스 한번 클릭
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if (rayHit.transform.tag == "PlayerUnit")
+                    {
+                        playerUnit = Instantiate(rayHit.transform.gameObject, mousePos, Quaternion.identity);
+                        playerUnit.name = rayHit.transform.name;
+                        playerUnit.transform.localScale = new Vector2(9f, 9f);
+                        playerUnit.GetComponent<BoxCollider2D>().enabled = false;
+                    }
+                }
+            }
+
+            if (playerUnit != null)
+            {
+                playerUnit.transform.position = mousePos;
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            if (playerUnit != null)
+            {
+                if (rayHit.transform == null || rayHit.transform.tag == "PlayerUnit")
+                {
+                    Destroy(playerUnit);
+                }
+                else
+                {
+                    //내려놓는 위치의 이름에 따라 지정되는 위치가 차이가 난다.
+                    switch (rayHit.transform.name)
+                    {
+                        case "Line1":
+                            playerUnit.transform.position = LineTfs[0].position;
+                            break;
+                        case "Line2":
+                            playerUnit.transform.position = LineTfs[1].position;
+                            break;
+                        case "Line3":
+                            playerUnit.transform.position = LineTfs[2].position;
+                            break;
+                        case "Line4":
+                            playerUnit.transform.position = LineTfs[3].position;
+                            break;
+                    }
+
+                    playerUnit.transform.tag = "Untagged";
+                    playerUnit.GetComponent<BoxCollider2D>().enabled = true;
+                    playerUnit.GetComponent<BoxCollider2D>().isTrigger = true;
+                    switch(playerUnit.name)
+                    {
+                        case "Sword":
+                            playerUnit.GetComponent<BoxCollider2D>().offset = new Vector2(0.2f, 0);
+                            //playerUnit.GetComponent
+                            break;
+                    }
+
+                    playerUnit.GetComponent<PlayerUnit>().enabled = true;
+                    playerUnit = null;
+
+                }//else 끝
+            }//비어 있지 않은가 체크 끝
+        }//마우스 떼었을떄 끝
+    }// update 끝
+}
